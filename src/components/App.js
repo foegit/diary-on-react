8c853b81b4e93b0f8364d1d0ddbe08c827e16875
Foegit <foegit@gmail.com>
2018-06-28 23:26:55 +0300

    Add basic react component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
+import SideBar from './SideBar';
+import Content from './Content';
 
 class App extends Component{
   render(){
-    return (<div>
-      <div className="sideBar">
-        <h1>Diary app</h1>
-        <h2>Comment with no sense</h2>
-      </div>
-      <div className="contentPage">
-        <div className="diaryItems panel"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt expedita similique autem, ab, aliquam, eos molestiae eum beatae distinctio cum voluptatibus nesciunt accusamus. Amet quam dolor illo deleniti, ratione harum.</div>
-        <div className="commentBlock panel"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat optio fugiat maxime perspiciatis quisquam accusantium totam animi deserunt cupiditate temporibus, ullam deleniti, asperiores culpa aliquid explicabo sint. Soluta, molestiae, veniam!</div>
-      </div>
-    </div>);
+    return (
+      <React.Fragment>
+        <SideBar/>
+        <Content/>
+    </React.Fragment>);
   }
 }
 export default App;
