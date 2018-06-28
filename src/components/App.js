916bd454218ea38c82c46e99d5c5d2ec6935c0f9
Foegit <foegit@gmail.com>
2018-06-28 22:05:04 +0300

    Add static sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component{
   render(){
-    return null;
+    return (<div>
+      <div className="sideBar">
+        <h1>Diary app</h1>
+        <h2>Comment with no sense</h2>
+      </div>
+      <div className="contentPage"></div>
+    </div>);
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './sass/style.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
